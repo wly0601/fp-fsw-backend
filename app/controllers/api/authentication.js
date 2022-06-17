71607f6754b202c03e28ad93c36f24478deb620e
Devi Indriyana Putri <deviindriyanaputri@gmail.com>
2022-06-17 20:32:09 +0700

    feat:login and change code in register

--- app/controllers/api/authentication.js
+++ app/controllers/api/authentication.js
@@ -1,44 +1,97 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userServices = require("../../services/users");
 
-function encryptPassword(password) {
-  return new Promise((resolve, reject) => {
-    bcrypt.hash(password, 10, (err, encryptedPassword) => {
-      if (!!err) {
-        reject(err);
-        return;
-      }
-
-      resolve(encryptedPassword);
-    });
-  });
+async function checkPassword(password, encryptedPassword) {
+  return await bcrypt.compareSync(password, encryptedPassword)
 }
 
-function checkPassword(encryptedPassword, password) {
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(password, encryptedPassword, (err, isPasswordCorrect) => {
-      if (!!err) {
-        reject(err);
-        return;
-      }
-
-      resolve(isPasswordCorrect);
-    });
-  });
+function createToken(payload) {
+  return jwt.sign(payload, process.env.JWT_SIGNATURE_KEY || "Token");
 }
 
 module.exports = {
-  async register(req, res) {
-    const {name, email, password} = req.body
-    const encryptedPassword = await encryptPassword(password);
-    const user = await userServices.create({ email,name, encryptedPassword });
-    res.status(201).json({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    });
+  async register(req, res, next) {
+    try {
+      const {
+        name,
+        email,
+        password
+      } = req.body
+      const encryptedPassword = await bcrypt.hash(password, 10)
+      const user = await userServices.create({
+        email: email.toLowerCase(),
+        name,
+        encryptedPassword
+      });
+      res.status(201).json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      });
+      next();
+    } catch (err) {
+      res.status(401).json({
+        status: "Failed",
+        message: err.message
+      });
+    }
   },
 
+  async login(req, res) {
+    try {
+      const email = req.body.email.toLowerCase();
+      const password = req.body.password;
+
+      const user = await userServices.getOne({
+        where: {
+          email
+        },
+      });
+
+      if (!user) {
+        res.status(404).json({
+          status:"Failed",
+          message:"Email not found!"
+        });
+        return
+      }
+
+      const isPasswordCorrect = await bcrypt.compareSync(password, user.encryptedPassword)
+
+      if (!isPasswordCorrect) {
+        res.status(401).json({
+          status:"Failed",
+          message: "Password is incorrect!"
+        });
+        return;
+      }
+
+      const token = createToken({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      }, process.env.JWT_PRIVATE_KEY || 'apahayo', {
+        expiresIn: '1h'
+      });
+
+      res.status(201).json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        token,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      });
+    } catch (err) {
+      res.status(401).json({
+        status: "Failed",
+        message: err.message
+      });
+    }
+  },
 };
--- app/router.js
+++ app/router.js
@@ -7,7 +7,8 @@ const apiRouter = express.Router();
 /**
  * Authentication Resource
  * */
-// apiRouter.post("/api/login", controllers.api.authentication.login);
+apiRouter.post("/api/login", controllers.api.authentication.login);
+
 apiRouter.post("/api/register",
     middlewares.checkCondition.checkCondition, 
     controllers.api.authentication.register
