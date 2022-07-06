5a00eb75ae992fe6f3dddc71099de90a842e5c16
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2022-07-06 16:40:59 +0700

    add: some progress at get notification

--- app/controllers/api/transaction.js
+++ app/controllers/api/transaction.js
@@ -1,19 +1,52 @@
 const transactionServices = require("../../services/transaction");
 const productServices = require("../../services/product");
+const {Products} = require("../../models")
 const {Op} = require("sequelize");
+
+function priceFormat(data) {
+		const priceStr = data.toString();
+		var i = priceStr.length;
+		var renderPrice = '';
+		var counter = 0;
+
+		while (i > 0) {
+			renderPrice = priceStr[i-1] + renderPrice;
+			i--;
+			counter++;
+			if (counter == 3 && i !== 0) {
+				renderPrice = '.' + renderPrice;
+				counter = 0;
+			}
+		}
+
+		return `Rp ${renderPrice}`;
+};
+
+function timeFormat(date){
+	const monthNames=["Jan", "Feb" , "Mar" , "Apr" , "May" , "Jun" ,"Jul", "Aug" , "Sep", "Oct" , "Nov" , "Dec" ];
+	if (date.getMinutes() < 10) {
+		var minutes = '0' + date.getMinutes().toString();
+	} else {
+		minutes = date.getMinutes();
+	}
+	
+	const timeRender = `${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}, ${date.getHours()}:${minutes}`
+
+	return timeRender;
+}
 
 module.exports = {
 	async createTransaction(req, res) {
 		try {
-            const {bargainPrice,productId} = req.body
-            const dateOfBargain = new Date()
+						const {bargainPrice,productId} = req.body
+						const dateOfBargain = new Date()
 			const product = await productServices.get(productId)
 
-            const transaction = await transactionServices.create({
+						const transaction = await transactionServices.create({
 				productId: product.id,
 				buyerId: req.user.id,
 				bargainPrice,
-                dateOfBargain,
+								dateOfBargain,
 			});
 
 			res.status(201).json(transaction);
@@ -45,25 +78,76 @@ module.exports = {
 					productId : {
 						[Op.or]: getProductSeller
 					}
+				},
+				include: {
+					model: Products,
 				}
 			})
 
 			const getTransactionBuyer = await transactionServices.listByCondition({
 				where: {
-					buyerId : req.params.id
+					buyerId : req.params.id,
+					accBySeller: {
+						[Op.or]: [true, false]
+					}
+				},
+				include: {
+					model: Products,
 				}
 			})
 
-			result = [].concat(
-				getProduct,
-				getTransactionSeller, 
-				getTransactionBuyer
+			results = [].concat(
+				getProduct.map((product) => {
+					return ({
+						information: "Product of this user.",
+						product 
+					})
+				}),
+				getTransactionSeller.map((ts) => {
+					return ({
+						information: "Product of this user that is bargained by someone else.",
+						ts
+					})
+				}), 
+				getTransactionBuyer.map((tb) => {
+					return ({
+						information: "Product of this user that want to buy.",
+						tb
+					})
+				})
 			)
 
-			const message = {}
+			const messages = results.map((result) => {
+				var msg, productId, image, name, price, time, information, show;
+				if(result.information === "Product of this user."){
+					show = result.product
+					return ({
+						msg: "Berhasil Diterbitkan",
+						productId: show.id,
+						image: show.images[0],
+						name: show.name,
+						price: priceFormat(show.price),
+						time: timeFormat(show.createdAt),
+						information: "Please GET /api/product/productId"
+					})
+				} else if (result.information === "Product of this user that is bargained by someone else."){
+					show = result.ts
+					return ({
+						msg: "Penawaran Produk",
+						transactionId: show.id,
+						image: show.Product.images[0],
+						name: show.Product.name,
+						price: priceFormat(show.Product.price),
+						bargainPrice: priceFormat(show.bargainPrice),
+						time: timeFormat(show.dateOfBargain),
+						information: "Please GET /api/transaction/transactionId"
+					})
+				} 
+			})
+
 			res.status(200).json({
 				status: "success",
-				data: result
+				data: messages
 			})
 		} catch (err) {
 			res.status(400).json({
