d752a0190662d31feb8438dfaa161e5be6d00879
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2023-08-10 15:36:33 +0700

    vercel serverless deploy

--- config/database.js
+++ config/database.js
@@ -20,7 +20,6 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: `${process.env.DB_DATABASE}`,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: "postgres",
     dialectModule: pg,
     dialectOptions: {
